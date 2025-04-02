45719c809a8cfe99403f6ecacbe29e79b3c95cec
s.maruoka <s.maruoka@caremax.co.jp>
2025-04-02 14:18:24 +0900

    外部PCからの接続ができるように修正

--- app.py
+++ app.py
@@ -110,4 +110,4 @@ def edit_knowledge(id):
     return render_template('edit.html', entry=entry, categories=categories)
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(host='0.0.0.0', port=5000, debug=True) 
